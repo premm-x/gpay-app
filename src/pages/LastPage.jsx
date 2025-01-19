8bc2581fbdd7ef88f7e6ccc6ddf6f05614bc46b9
premm <mybrother1370@gmail.com>
2025-01-19 22:11:18 +0530

    change for deploy

--- src/pages/LastPage.jsx
+++ src/pages/LastPage.jsx
@@ -4,7 +4,7 @@ const LastPage = () => {
     return (
         <div className='py-10 px-8'>
             <h1 className='text-2xl mb-8'>Bank balance</h1>
-            <h1 className='text-3xl mb-6'>₹56.23</h1>
+            <h1 className='text-3xl mb-6'>₹26.23</h1>
             <p className='text-lg text-gray-400'>Saving account</p>
             <p className='text-lg text-gray-400'>Bank Of Maharashtra 7898</p>
         </div>
